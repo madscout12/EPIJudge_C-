b93fa92d814fa1edbc27f93d6e0190559e14f7f6
duppy-conqueror <zkoopmans@gmail.com>
2018-05-25 13:13:54 -0500

    4.1 Complete: Committing working parity code.

--- epi_judge_cpp/parity.cc
+++ epi_judge_cpp/parity.cc
@@ -1,7 +1,50 @@
+#include <climits>
 #include "test_framework/generic_test.h"
 short Parity(unsigned long long x) {
   // TODO - you fill in here.
-  return 0;
+
+  size_t shift = sizeof(unsigned long long) * 8 / 2;
+  unsigned long long max = ULLONG_MAX >> shift;
+
+
+  while(x && shift > 0){
+      x = (x >> shift) ^ (x & max);
+      shift = shift / 2;
+      max = max >> shift;
+  }
+
+  if(shift > 0) return 0;
+  else{
+      return (short) x;
+  }
+
+
+  /*
+  int parity = 0;
+
+  while(x){
+      parity += x & 1;
+      x = x >> 1;
+  }
+
+  return (short) parity % 2;
+
+  */
+
+  /*
+  while(shift > 1 && x != 0){
+    unsigned long long top = x >> shift;
+    x = mask & x;
+    x = x ^ top;
+    shift /= 2;
+    mask = mask >> shift;
+  }
+
+
+  if(x) return (short) 1;
+  else return (short) 0;
+
+   */
 }
 
 int main(int argc, char* argv[]) {
